c29cc54d226290136cee00fd5365c37f87458ef4
Razi Yahya <86314686+raziyahya@users.noreply.github.com>
2023-08-15 15:13:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,194 @@
+# import modules
+
+from tkinter import *
+import os
+
+# Designing window for registration
+from subprocess import call
+
+
+def register():
+    global register_screen
+    register_screen = Toplevel(main_screen)
+    register_screen.title("Register")
+    width=register_screen.winfo_screenwidth()
+    height=register_screen.winfo_screenheight()
+    register_screen.geometry("%dx%d" % (width,height))
+
+    global username
+    global password
+    global username_entry
+    global password_entry
+    username = StringVar()
+    password = StringVar()
+
+    Label(register_screen, text="Please enter details below",fg="white", bg="black", width="300", height="2", font=("Calibri", 15)).pack()
+    Label(register_screen, text="").pack()
+    username_lable = Label(register_screen, text="Username * ")
+    username_lable.pack()
+    username_entry = Entry(register_screen, textvariable=username)
+    username_entry.pack()
+    password_lable = Label(register_screen, text="Password * ")
+    password_lable.pack()
+    password_entry = Entry(register_screen, textvariable=password, show='*')
+    password_entry.pack()
+    Label(register_screen, text="").pack()
+    Button(register_screen, text="Register", width=10, height=1, bg="black",fg="white", command=register_user).pack()
+
+
+# Designing window for login 
+
+def login():
+    global login_screen
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+    width=login_screen.winfo_screenwidth()
+    height=login_screen.winfo_screenheight()
+    login_screen.geometry("%dx%d" % (width,height))
+    Label(login_screen, text="Please enter details below to login",fg="white",bg="black", width="300", height="2", font=("Calibri", 15)).pack()
+    Label(login_screen, text="").pack()
+
+    global username_verify
+    global password_verify
+
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    global username_login_entry
+    global password_login_entry
+
+    Label(login_screen, text="Username * ").pack()
+    username_login_entry = Entry(login_screen, textvariable=username_verify)
+    username_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Password * ").pack()
+    password_login_entry = Entry(login_screen, textvariable=password_verify, show='*')
+    password_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Button(login_screen, text="Login", width=10, height=1, command=login_verify).pack()
+
+
+# Implementing event on register button
+
+def register_user():
+    username_info = username.get()
+    password_info = password.get()
+
+    file = open(username_info, "w")
+    file.write(username_info + "\n")
+    file.write(password_info)
+    file.close()
+
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+    Label(register_screen, text="Registration Success", fg="black", font=("calibri", 11)).pack()
+
+
+# Implementing event on login button 
+
+
+def login_verify():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_login_entry.delete(0, END)
+    password_login_entry.delete(0, END)
+
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1, "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_sucess()
+
+        else:
+            password_not_recognised()
+
+    else:
+        user_not_found()
+
+def call_python_file():
+    call(["Python", "{}".format(r'C:\Users\razia\PycharmProjects\Facial-Recognition-for-Crime-Detection\home.py')])
+
+# Designing popup for login success
+
+def login_sucess():
+
+    call_python_file()
+
+
+
+# Designing popup for login invalid password
+
+def password_not_recognised():
+    global password_not_recog_screen
+    password_not_recog_screen = Toplevel(login_screen)
+    password_not_recog_screen.title("Success")
+    password_not_recog_screen.geometry("150x100")
+    Label(password_not_recog_screen, text="Invalid Password ").pack()
+    Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
+
+
+# Designing popup for user not found
+
+def user_not_found():
+    global user_not_found_screen
+    user_not_found_screen = Toplevel(login_screen)
+    user_not_found_screen.title("Success")
+    user_not_found_screen.geometry("150x100")
+    Label(user_not_found_screen, text="User Not Found").pack()
+    Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
+
+
+# Deleting popups
+
+def delete_login_success():
+    login_success_screen.destroy()
+
+
+def delete_password_not_recognised():
+    password_not_recog_screen.destroy()
+
+
+def delete_user_not_found_screen():
+    user_not_found_screen.destroy()
+
+
+# Designing Main(first) window
+
+def main_account_screen():
+    global main_screen
+    main_screen = Tk()
+    width=main_screen.winfo_screenwidth()
+    height=main_screen.winfo_screenheight()
+    main_screen.geometry("%dx%d" % (width,height))
+    main_screen.title("Account Login")
+    Label(text="CRIME INVESTIGATION DEPARTMENT", fg="#c0c0c0", bg="BLACK",
+      font="Arial 35 bold", padx=600).pack(side='top')
+    Label(text="CRIME INVESTIGATION DEPARTMENT", fg="#c0c0c0", bg="BLACK",
+      font="Arial 35 bold", padx=600).pack(side='bottom')
+    Label(text="").pack()
+    Label(text="").pack()
+    Label(text="").pack()
+    Label(text="").pack()
+    Label(text="Select Your Choice",fg="black", width="300", height="2", font=("Calibri", 15)).pack()
+    Label(text="").pack()
+    
+    
+
+
+    Button(text="Login",height="2", width="30", command = login,bg="black",fg="white",font=("Calibri", 13)).pack()
+    Label(text="").pack()
+    Label(text="").pack()
+
+
+    Button(text="Register",height="2", width="30", command=register,bg="black",fg="white",font=("Calibri", 13)).pack()
+
+    main_screen.mainloop()
+
+
+
+
+
+
+main_account_screen()
